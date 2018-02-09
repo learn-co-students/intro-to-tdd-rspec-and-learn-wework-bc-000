e4fcf94f2d8bfbec80491d4068f2ec9a117acefe
Anne Moxie <anne.moxie@wework.com>
2018-02-09 00:36:33 +0000

    Done.

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,13 @@
+#fill out profile and year they were born
+
+#year_now = 2003
+#year_born = 1984
+
+year = 1984
+
+def current_age_for_birth_year(birth_year)
+  2003 - birth_year
+end
+
+current_age_for_birth_year(year)
+  
