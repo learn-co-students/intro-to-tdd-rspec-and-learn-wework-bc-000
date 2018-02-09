c523c9226584baaaaf3c9b449da246f0efbaae1d
Jesse Ganes <jesse.ganes@wework.com>
2018-02-09 00:50:03 +0000

    Done.

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,16 @@
+# year = 1984
+
+# def current_age_for_birth_year(bananas)
+#   puts bananas
+# end
+
+# age_of_person = 2003 - current_age_for_birth_year(year)
+
+
+
+def current_age_for_birth_year(year)
+age_of_person = 2003 - year
+end
+
+
+
